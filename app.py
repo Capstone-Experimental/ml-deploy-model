7f8bb5b52b9fd1223bab3182bdce94a7906787cf
zidane-arkan <zidark897@Gmail.com>
2023-12-11 10:41:32 +0700

    Update app.py v1.1
    Create post api for predictions

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # Import the Flask class from the flask module
+import jsonify
 from flask import Flask, render_template, request
+from utilities import load_predict_model
 
 # Create an instance of the Flask class
 app = Flask(__name__)
@@ -11,6 +13,22 @@ def home():
     if request.method == 'POST':
         text = request.form.get('text-content')
     return render_template("index.html", text=text)
+
+# Predict Texts Sentiment
+@app.post('/predict')
+def predict():
+    data = request.json
+    try :
+        sample = data['text']
+    except KeyError:
+        return jsonify({'error' : 'No text sent'})
+    sample = [sample]
+    predictions = load_predict_model(sample)
+    try : 
+        result = jsonify(predictions[0])    
+    except TypeError as e:
+        result = jsonify({'error': str(e)})
+    return result
 
 # Run the Flask application
 if __name__ == '__main__':
